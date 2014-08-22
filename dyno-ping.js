a45c528f89774762d2182dacc5a3ee37ca7d2516
Robin Hawkes <rob@rawkes.com>
2014-08-22 15:49:25 +0100

    Tweaked dyno ping

--- dyno-ping.js
+++ dyno-ping.js
@@ -1,5 +1,7 @@
 var request = require("request");
 
 if (process.env.PING_URL) {
-  request(process.env.PING_URL, function(error, response, body) {});
+  request(process.env.PING_URL, function(error, response, body) {
+    console.log(response);
+  });
 }
